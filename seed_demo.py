c2a23d4fdacababb6c5e0b080517e9ada20de38b
Santiago Suarez <43580941@terciariourquiza.edu.ar>
2026-04-30 11:21:34 -0300

    Corrijo error de bycrpt, paso a argon2

--- seed_demo.py
+++ seed_demo.py
@@ -3,7 +3,7 @@ from app.models.company import Company
 from app.models.user import User
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def seed_demo():
     db = SessionLocal()
